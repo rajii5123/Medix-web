9aa8c8edd657d5af921fb1e8148ac7415368f5f9
rajii5123 <rajii5123.a@gmail.com>
2026-04-07 19:11:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const API = "http://127.0.0.1:5000"; // change later
+
+async function uploadHuman() {
+    let file = document.getElementById('humanImg').files[0];
+    alert("Frontend working!");
+}
+
+async function uploadPlant() {
+    let file = document.getElementById('plantImg').files[0];
+    alert("Frontend working!");
+}
+
+async function uploadReport() {
+    let file = document.getElementById('reportFile').files[0];
+    alert("Frontend working!");
+}
