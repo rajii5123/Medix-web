b8e6993771a0c1d567ee000e3827c94d0c65e2b9
rajii5123 <rajii5123.a@gmail.com>
2026-04-08 00:51:36 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,1 @@
-function login(){
-    const user = document.getElementById('username').value;
-    const pass = document.getElementById('password').value;
 
-    if(user === "admin" && pass === "admin"){
-        window.location.href = "dashboard.html";
-    } else {
-        const msg = document.getElementById('login-msg');
-        msg.innerText = "Invalid username or password";
-        msg.style.color = "red";
-    }
-}
