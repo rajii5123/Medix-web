0bb466bc03c78240dca2ac3fbde9bc8cfd8a14f0
rajii5123 <rajii5123.a@gmail.com>
2026-04-07 19:34:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-const API = "http://127.0.0.1:5000"; // change later
+const API = "https://medix-web.onrender.com";
 
 async function uploadHuman() {
     let file = document.getElementById('humanImg').files[0];
-    alert("Frontend working!");
+    let formData = new FormData();
+    formData.append("image", file);
+
+    let res = await fetch(API + "/predict/human", {
+        method: "POST",
+        body: formData
+    });
+
+    let data = await res.json();
+    document.getElementById("result").innerText = data.result;
 }
 
 async function uploadPlant() {
     let file = document.getElementById('plantImg').files[0];
-    alert("Frontend working!");
+    let formData = new FormData();
+    formData.append("image", file);
+
+    let res = await fetch(API + "/predict/plant", {
+        method: "POST",
+        body: formData
+    });
+
+    let data = await res.json();
+    document.getElementById("result").innerText = data.result;
 }
 
 async function uploadReport() {
     let file = document.getElementById('reportFile').files[0];
-    alert("Frontend working!");
+    let formData = new FormData();
+    formData.append("file", file);
+
+    let res = await fetch(API + "/analyze/report", {
+        method: "POST",
+        body: formData
+    });
+
+    let data = await res.json();
+    document.getElementById("result").innerText = data.analysis;
 }
